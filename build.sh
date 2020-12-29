ab42d48420a0c138d29b83b4a41f5fb84a5da376
Linh B Ngo <lngo@wcupa.edu>
2020-12-29 10:50:46 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ shopt -s expand_aliases
 
 alias docker='podman'
 
+sed "s/myuser/${USER}/g" Dockerfile.template > Dockerfile.current
+
 docker image rm csc-container | true
-docker build -t csc-container:latest .
+docker build -t csc-container:latest -f Dockerfile.current
 
