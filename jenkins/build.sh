e835cf3cf35bbcc770028922520dc2d26ca3bc50
Linh B. Ngo <lngo@wcupa.edu>
2021-04-11 13:12:25 -0400

    add jenkins

---
+++ jenkins/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+shopt -s expand_aliases
+
+if ! type docker > /dev/null; then
+  alias docker='podman'
+fi
+
+sed "s/myuser/${USER}/g" Dockerfile.template > Dockerfile.current
+
+docker image rm csc-container | true
+docker build -t csc-container:latest -f Dockerfile.current .
+
