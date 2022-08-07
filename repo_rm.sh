6f1454cd7b32682b4e27a3ddcdad8161e6310792
Linh B. Ngo <lngo@wcupa.edu>
2022-08-07 16:43:15 -0400

    add repo

--- repo_rm.sh
+++ repo_rm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for phrase in "lab-1-" "lab-2-" "lab-3-" "lab-4-" "lab-5-" "assignment-1-" "assignment-2-" "assignment-3-" "assignment-4-" "assignment-5-"; do
+for phrase in "lab-1-" "lab-2-" "lab-3-" "lab-4-" "lab-5-" "assignment-1-" "assignment-2-" "assignment-3-" "assignment-4-" "assignment-5-" "intro-to-github"; do
   while read -r line; do 
     repo=`echo $line | grep $phrase`
     if [[ $repo != "" ]]; then
       echo $repo
-      gh repo-delete $repo
+      gh repo delete $repo --confirm
     fi
-  done < <(gh repos CSC231-WCU)
+  done < <(gh repo list  CSC231-WCU | awk '{print $1;}')
 done
